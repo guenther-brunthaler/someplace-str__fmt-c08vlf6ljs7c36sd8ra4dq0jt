350b4782d0849ca03199244e85b07d97dbc9d570
Guenther Brunthaler <gb@xrpi3.emgenxx69lwyn5ctlr4nl64ul.local>
2018-08-16 08:30:41 +0200

    Better error reporting by formatting function
    Provide a particular error message to the caller.

--- fmt.c
+++ fmt.c
@@ -21,9 +21,11 @@ static char *sfmt_helper2(struct isq *fmt) {
          int key= *++fstr;
          for (search= fmt->parent; ; search= search->parent) {
             if (!search) {
-               fail:
                free(buffer);
-               return 0;
+               return
+                  (char *)"\0"
+                  "Undefined insertion sequence referenced in format string!"
+               ;
             }
             if (search->key == key) break;
          }
@@ -37,13 +39,17 @@ static char *sfmt_helper2(struct isq *fmt) {
          char *nbuf;
          if (!bsz) bsz= 80;
          while (used + isz >= bsz) bsz+= bsz;
-         if (!(nbuf= realloc(buffer, bsz))) goto fail;
+         if (!(nbuf= realloc(buffer, bsz))) {
+            realloc_failure:
+            free(buffer);
+            return (char *)"\0" "Memory allocation failure!";
+         }
          buffer= nbuf;
       }
       (void)memcpy(buffer + used, insert, isz); used+= isz;
       if (!*fstr) {
          char *final;
-         if (!(final= realloc(buffer, used))) goto fail;
+         if (!(final= realloc(buffer, used))) goto realloc_failure;
          return final;
       }
       ++fstr;
@@ -59,29 +65,58 @@ static char *sfmt_helper(struct isq *sq, va_list *args) {
    }
 }
 
-static char *sfmt(int key_1, ...) {
-   struct isq root= {0, key_1};
-   char *result;
+/* Expand a simple format string containing string insertion sequences into a
+ * dynamically alloated buffer.
+ *
+ * <result> is the address of a text pointer where the result will be stored.
+ *
+ * <key_1> and <expansion_1> are the first pair of char/string pairs which
+ * define all available insertion sequences as well as the format string.
+ *
+ * Insertion sequences come first in arbitrary order and define a "char" key
+ * and an expansion string associated with the key.
+ *
+ * A key of 0 is special and its associated insertion string then actually
+ * represents the format string, which always has to be the last argument.
+ *
+ * Within the format string, all characters are copied literally to the output
+ * buffer except sequences of the form "%" followed by some character. This
+ * represents an insertion sequence, and the character following the "%" will
+ * be used as a lookup key for finding the expansion string to insert.
+ *
+ * There is no built-in way to output a literal "%", but if required an
+ * expansion can be defined to insert it (e. g. one with a key of '%' which
+ * will insert "%").
+ *
+ * Return value: If the function returns non-zero then *<result> contains a
+ * pointer to the successfully dynamically allocated string. It is the
+ * responsibility of the caller to free() this string eventually.
+ *
+ * If the function returns 0, then the format operation has failed and
+ * *<result> contains a read-only statically allocated error message which
+ * must not be freed. */
+static int sfmt(char **result, int key_1, const char *expansion_1, ...) {
+   struct isq root= {0, key_1, expansion_1};
    va_list args;
-   va_start(args, key_1);
-   root.expansion= va_arg(args, char const *);
-   result= sfmt_helper(&root, &args);
+   va_start(args, expansion_1);
+   *result= sfmt_helper(&root, &args);
    va_end(args);
-   return result;
+   if (*result) return 1;
+   ++*result;
+   return 0;
 }
 
 int main(void) {
    char *str;
    char const *error= 0;
    if (
-      !(
-         str= sfmt(
-               'Y', "2001", 'M', "12", 'D', "24", 'w', "Santa Claus", 'm'
-            ,  "Ho Ho Ho", 0, "On %Y-%M-%D, %w said %m."
-         )
+      !sfmt(
+            &str
+         ,  'Y', "2001", 'M', "12", 'D', "24", 'w', "Santa Claus", 'm'
+         ,  "Ho Ho Ho", 0, "On %Y-%M-%D, %w said %m."
       )
    ) {
-      error= "string formatting error!";
+      error= str;
       fail:
       (void)fputs(error, stderr);
       (void)fputc('\n', stderr);
